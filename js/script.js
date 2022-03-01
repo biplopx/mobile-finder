9a85b71b2d171e7506a7abc8b6335196262ac7ab
MD Biplop Hossain <developerbiplop@gmail.com>
2022-03-01 12:46:06 +0600

    search feature addedt

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const searchPhone = () => {
+  const searchInput = document.getElementById('search-field');
+  searchText = searchInput.value;
+  searchInput.value = "";
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult(data.data));
+}
+
+const displaySearchResult = resultData => {
+  const productContainer = document.getElementById('product-container');
+  // for (const result of resultData) {
+  //   console.log(result.phone_name);
+  // }
+  resultData.forEach(productData => {
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="col">
+    <div class="card h-100">
+      <img src="${productData.image}" width="300px" class="img-thumbnail d-block mx-auto mt-3" alt="${productData.phone_name}">
+      <div class="card-body">
+        <h5 class="card-title">${productData.phone_name}</h5>
+        <p class="card-text">Brand: ${productData.brand}</p>
+      </div>
+      <div class="card-footer">
+      <button type="button" class="btn btn-primary d-block w-100">Details</button>
+      </div>
+    </div>
+  </div>
+    `;
+    productContainer.appendChild(div);
+  });
+  console.log(resultData)
+}
